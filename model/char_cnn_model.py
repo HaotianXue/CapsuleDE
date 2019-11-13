0809c0c89bbed0915d733bd7dbbf1a362d85a8cf
HaoTianXue <haotianxue@HaoTianXuedeMacBook-Pro.local>
2019-11-13 16:28:05 -0500

    char cnn model added

--- model/char_cnn_model.py
+++ model/char_cnn_model.py
@@ -18,8 +18,7 @@ class CharCnnModel(SenTensorModel):
                  hyper_parameter,
                  train_requirement,
                  is_gpu=torch.cuda.is_available(),
-                 model_save_path="../trained_model/char_cnn_model.pt",
-                 lr=5e-4):
+                 model_save_path="../trained_model/char_cnn_model.pt"):
         super(CharCnnModel, self).__init__(train_data_set,
                                            test_data_set,
                                            hyper_parameter,
@@ -33,11 +32,10 @@ class CharCnnModel(SenTensorModel):
         # self.load_test()
 
     def build_model(self):
-        d_w, num_filter, window_sizes, dropout_p = self.extract_hyper_parameters()
+        d_w, num_filter, dropout_p = self.extract_hyper_parameters()
         print("-----Start building model-----")
         model = CharCnnModelHelper(d_w,
                                    num_filter,
-                                   window_sizes,
                                    dropout_p,
                                    self.is_gpu)
         print("-----Finish building model-----")
@@ -46,32 +44,104 @@ class CharCnnModel(SenTensorModel):
     def extract_hyper_parameters(self):
         return self.hyper_parameter["d_w"], \
                self.hyper_parameter["num_filter"], \
-               self.hyper_parameter["window_size"], \
                self.hyper_parameter["dropout_p"]
 
 
 class CharCnnModelHelper(nn.Module):
 
-    def __init__(self, d_w, num_filter, window_sizes, dropout_p, is_gpu, num_classes=2):
+    def __init__(self, d_w, num_filter, dropout_p, is_gpu):
         super(CharCnnModelHelper, self).__init__()
-        self.w2v = nn.Embedding(97, d_w)  # char embedding
-        self.cnn_layer = CNNLayers(d_w, num_filter, window_sizes, dropout_p, is_gpu)
-        self.linear_layer = nn.Sequential(
-            nn.Linear(num_filter * len(window_sizes), num_filter),
+        self.w2v = nn.Embedding(97, d_w)
+        self.is_gpu = is_gpu
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, num_filter, kernel_size=(7, d_w), stride=1),
+            nn.ReLU()
+        )
+        self.conv1.apply(self.weights_init)
+
+        self.maxpool1 = nn.MaxPool2d(kernel_size=(3, 1), stride=(3, 1))
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(1, num_filter, kernel_size=(7, num_filter), stride=1),
+            nn.ReLU()
+        )
+        self.conv2.apply(self.weights_init)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=(3, 1), stride=(3, 1))
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(1, num_filter, kernel_size=(3, num_filter), stride=1),
+            nn.ReLU()
+        )
+        self.conv3.apply(self.weights_init)
+
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(1, num_filter, kernel_size=(3, num_filter), stride=1),
+            nn.ReLU()
+        )
+        self.conv4.apply(self.weights_init)
+
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(1, num_filter, kernel_size=(3, num_filter), stride=1),
+            nn.ReLU()
+        )
+        self.conv5.apply(self.weights_init)
+
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(1, num_filter, kernel_size=(3, num_filter), stride=1),
+            nn.ReLU()
+        )
+        self.conv6.apply(self.weights_init)
+
+        self.maxpool6 = nn.MaxPool2d(kernel_size=(3, 1), stride=(3, 1))
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(34 * num_filter, 1024),
             nn.ReLU(),
-            nn.Linear(num_filter, num_filter // 2),
+            nn.Dropout(p=dropout_p)
+        )
+        self.fc1.apply(self.weights_init)
+        self.fc2 = nn.Sequential(
+            nn.Linear(1024, 1024),
             nn.ReLU(),
-            nn.Linear(num_filter // 2, num_classes)
-        )  # out_shape: (batch_size, num_classes)
-        self.linear_layer.apply(self.weights_init)
+            nn.Dropout(p=dropout_p)
+        )
+        self.fc2.apply(self.weights_init)
+
+        self.fc3 = nn.Linear(1024, 2)
+        self.fc3.apply(self.weights_init)
 
     def forward(self, x):
         x = self.w2v(x)  # (batch_size, max_sen_len, d_w)
         x = torch.unsqueeze(x, dim=1)  # (batch_size, 1, max_sen_len, d_w)
-        out = self.cnn_layer(x)
-        m = nn.Tanh()
-        out = m(out)
-        out = self.linear_layer(out)  # (batch_size, 2)
+
+        x = self.conv1(x)
+        x = x.transpose(1, 3)
+        x = self.maxpool1(x)
+
+        x = self.conv2(x)
+        x = x.transpose(1, 3)
+        x = self.maxpool2(x)
+
+        x = self.conv3(x)
+        x = x.transpose(1, 3)
+
+        x = self.conv4(x)
+        x = x.transpose(1, 3)
+
+        x = self.conv5(x)
+        x = x.transpose(1, 3)
+
+        x = self.conv6(x)
+        x = x.transpose(1, 3)
+        x = self.maxpool6(x)
+
+        x = x.view(x.size(0), -1)
+
+        x = self.fc1(x)
+
+        x = self.fc2(x)
+
+        out = self.fc3(x)
         return out
 
     # method to initialize the model weights (in order to improve performance)
@@ -80,38 +150,6 @@ class CharCnnModelHelper(nn.Module):
             nn.init.xavier_uniform_(m.weight)
             if m.bias is not None:
                 nn.init.zeros_(m.bias)
-
-
-class CNNLayers(nn.Module):
-
-    def __init__(self, d_w, num_filter, window_sizes, dropout_p, is_gpu):
-        super(CNNLayers, self).__init__()
-        self.is_gpu = is_gpu
-        self.cnn_layers = []
-        for window_size in window_sizes:
-            cnn_layer = nn.Sequential(
-                nn.Conv2d(in_channels=1,
-                          out_channels=num_filter,
-                          kernel_size=(window_size, d_w),
-                          stride=(1, 1),
-                          padding=(0, 0)),  # (batch, num_filter, max_sen_len - window_size + 1, 1)
-                nn.MaxPool2d(kernel_size=(842 - window_size + 1, 1),
-                             stride=(1, 1)),  # (batch, num_filter, 1, 1)
-                nn.Dropout(dropout_p),
-            )
-            cnn_layer.apply(self.weights_init)
-            self.cnn_layers.append(cnn_layer)
-
-    def forward(self, x):
-        out_list = []
-        for cnn_layer in self.cnn_layers:
-            if self.is_gpu:
-                cnn_layer = cnn_layer.cuda()
-            out = cnn_layer(x)  # (batch_size, num_filter, 1, 1)
-            out = out.view(out.shape[0], -1)  # (batch_size, num_filter)
-            out_list.append(out)
-        out = torch.cat(out_list, dim=1)  # (batch_size, num_filter * len(out_list))
-        return out
 
     # method to initialize the model weights (in order to improve performance)
     def weights_init(self, m):
@@ -123,8 +161,8 @@ class CNNLayers(nn.Module):
 
 if __name__ == "__main__":
     from data_fetcher.dataFetcher import CharSemEvalDataSet
-    train_requirement = {"num_epoch": 1, "batch_size": 32, "lr": 1e-4}
-    hyper_parameter = {"d_w": 32, "num_filter": 64, "window_size": [1, 2, 3, 4, 5, 6, 7, 8], "dropout_p": 0.4}
-    train_data_set = CharSemEvalDataSet("../data/train.txt", None, 50, True)
-    test_data_set = CharSemEvalDataSet("../data/test.txt", None, 50, True, 842)
+    train_requirement = {"num_epoch": 1, "batch_size": 32, "lr": 3e-4}
+    hyper_parameter = {"d_w": 32, "num_filter": 256, "dropout_p": 0.5}
+    train_data_set = CharSemEvalDataSet("../data/train.txt", None, 50, True, 1014)
+    test_data_set = CharSemEvalDataSet("../data/test.txt", None, 50, True, 1014)
     model = CharCnnModel(train_data_set, test_data_set, hyper_parameter, train_requirement)
